9f3413558a6aa71a0c8bc0539e1c6fc99e2d2eb8
Art.Stea1th (Stanislav Kuzmich) <art.stea1th@gmail.com>
2016-12-04 23:48:43 +0300

    #3 impl. FindTransform(), find: offset X, Y, angle

--- SLAM/SLAM.Models/Map/MatrixBasedMapper.cs
+++ SLAM/SLAM.Models/Map/MatrixBasedMapper.cs
@@ -9,72 +9,129 @@ namespace SLAM.Models.Map {
 
     internal sealed class MatrixBasedMapper : BaseMaper {        
 
-        protected Point[] prevPointsBuffer;
-        protected Point[] nextPointsBuffer;
+        protected Point[] previousPointsBuffer;
+        protected Point[] currentPointsBuffer;
 
-        protected Point[] tmpPointsBuffer;
+        protected Point[] estimatedPointsBuffer;
 
-        private Matrix matrix;
+        //private Matrix matrix;
+
+        int maxHitPoints;
 
         private double translateXRange, translateYRange, rotateAngleRange;
         private double translateXStep, translateYStep, rotateAngleStep;
         private double translateX = 0.0, translateY = 0.0, rotateAngle = 0.0;
 
         public MatrixBasedMapper(DataProvider dataProvider) : base(dataProvider) {
-            Configure();
+            Configure(5.0, 1.0, 90.0, 30.0);
         }
 
-        private void Configure() {
+        private void Configure(double xKmpH, double yKmpH, double degreePerSec, double fps) {
 
-            translateXStep = 0.2;
-            translateYStep = 0.2;
-            rotateAngleStep = 0.2;
+            translateXRange = PixelPerFrameBySpeed(xKmpH, fps);
+            translateYRange = PixelPerFrameBySpeed(yKmpH, fps);
+            rotateAngleRange = DegreePerFrame(degreePerSec, fps);
 
-            translateXRange = 12.0;
-            translateYRange = 12.0;
-            rotateAngleRange = 20.0;            
+            translateXStep = 1.0;
+            translateYStep = 1.0;
+            rotateAngleStep = 0.5;
+        }
+
+        private double PixelPerFrameBySpeed(double speedKmH, double fps) {
+            double mPerH = speedKmH * 1000.0;
+            double cmPerH = speedKmH * 100.0; 
+            double cmPerMin = cmPerH * 0.6;
+            double cmPerSec = cmPerMin * 0.6;
+            double cmPerFrame = cmPerSec * (1.0 / fps);
+            return cmPerFrame; // <-- 1 cm = 1 pixel
+        }
+        private double DegreePerFrame(double degreePerSec, double fps) {
+            return degreePerSec * (1.0 / fps);
         }
 
         protected override void NextFrameProceed() {
-            prevPointsBuffer = nextPointsBuffer;
-            DataProvider.GetNextFrameTo(out nextPointsBuffer);
-            TempBufferInitialize();
+            previousPointsBuffer = currentPointsBuffer;
             ResetTransform();
+            DataProvider.GetNextFrameTo(out currentPointsBuffer);
+            if (EstimatedBufferInitialize()) {                
+                FindTransform();
+            }            
         }
 
-        private void TempBufferInitialize() {
-            if (tmpPointsBuffer == null) {
-                tmpPointsBuffer = new Point[nextPointsBuffer.Length];
+        private bool EstimatedBufferInitialize() {
+            if (previousPointsBuffer == null) {
+                return false;
+            }
+            if (estimatedPointsBuffer == null) {
+                estimatedPointsBuffer = new Point[previousPointsBuffer.Length];
             }            
-            Array.Copy(nextPointsBuffer, tmpPointsBuffer, tmpPointsBuffer.Length);           
+            Array.Copy(previousPointsBuffer, estimatedPointsBuffer, estimatedPointsBuffer.Length);
+            return true;
         }
         
         private void ResetTransform() {
-            matrix = new Matrix();
+            //matrix = new Matrix();
             translateX = translateY = rotateAngle = 0.0;
+            maxHitPoints = 0;
         }
 
         private void FindTransform() {
 
             // prepare points
-            ApplyTransform(tmpPointsBuffer, translateXRange * -0.5, translateYRange * -0.5, rotateAngleRange * -0.5);
+            //ApplyTransform(estimatedPointsBuffer, translateXRange * -0.5, translateYRange * -0.5, rotateAngleRange * -0.5);
 
-            for (double x = 0.0; x <= translateX; x += translateXStep) {
-                for (double y = 0.0; y <= translateYRange; y += translateYStep) {
-                    for (double angle = 0.0; angle <= rotateAngleRange; angle += rotateAngleStep) {
+            //int maxHitPoints = 0;
 
+            maxHitPoints = HitPointsCount(estimatedPointsBuffer, previousPointsBuffer);
+
+            for (double x = -translateXRange; x <= translateXRange; x += translateXStep) {
+                for (double y = -translateYRange; y <= translateYRange; y += translateYStep) {
+                    for (double angle = -rotateAngleRange; angle <= rotateAngleRange; angle += rotateAngleStep) {
+
+                        ApplyTransform(estimatedPointsBuffer, x, y, angle);
+
+                        int currentHitPoints = HitPointsCount(estimatedPointsBuffer, currentPointsBuffer);
+                        if (currentHitPoints > maxHitPoints) {
+                            maxHitPoints = currentHitPoints;
+                            translateX = x; translateY = y; rotateAngle = angle;
+                        }
+                        Array.Copy(previousPointsBuffer, estimatedPointsBuffer, estimatedPointsBuffer.Length); // reset
                     }
                 }
             }
         }
 
         private void ApplyTransform(Point[] points, double offsetX, double offsetY, double angle) {
+
+            Matrix matrix = new Matrix();
+            double centerX = DataProvider.FrameInfo.Width * 0.5;
+            double centerY = DataProvider.FrameInfo.Height - 1;
+            matrix.RotateAt(angle, centerX, centerY);
+            matrix.Transform(points);
+
             for (int i = 0; i < points.Length; ++i) {
                 points[i].X += offsetX;
                 points[i].Y += offsetY;
+            }            
+        }
+
+        private int HitPointsCount(Point[] pointsBufferA, Point[] pointsBufferB) {
+
+            int resultHits = 0;
+            for (int i = 0; i < pointsBufferA.Length; ++i) {
+                for (int j = 0; j < pointsBufferB.Length; ++j) {
+                    if (HitPoint(pointsBufferA[i], pointsBufferB[j])) {
+                        ++resultHits;
+                    }
+                }
             }
-            matrix.RotateAt(angle, DataProvider.FrameInfo.Width * 0.5, DataProvider.FrameInfo.Height - 1);
-            matrix.Transform(points);
+            return resultHits;
+        }
+
+        private bool HitPoint(Point pointA, Point pointB) {
+            return
+                Math.Abs(pointA.X - pointB.X) < translateXStep &&
+                Math.Abs(pointA.Y - pointB.Y) < translateYStep;
         }
     }
 }
