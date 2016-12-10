5a0d9d708eb9a375172d69773c618e7965678797
Art.Stea1th (Stanislav Kuzmich) <art.stea1th@gmail.com>
2016-12-10 05:28:18 +0300

    #4 viewport customize, part 1 (axis visible, data and axis colors)

--- SLAM/SLAM.Views/Controls/Custom/MathDataViewport.cs
+++ SLAM/SLAM.Views/Controls/Custom/MathDataViewport.cs
@@ -1,17 +1,20 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
-
+using System.Windows.Shapes;
 
 namespace SLAM.Views.Controls.Custom {
 
     [TemplatePart(Name = ViewportSurfaceName, Type = typeof(Image))]
-    public class MathDataViewport : Control {
+    public sealed class MathDataViewport : Control {
 
         #region TemplateParts
 
         private const string ViewportSurfaceName = "PART_ViewportSurface";
+        private const string XAxisName = "PART_Axis";
+
         private Image viewportSurface;
+        private Path axis;
 
         #endregion
 
@@ -19,6 +22,10 @@ namespace SLAM.Views.Controls.Custom {
 
         public static readonly DependencyProperty DataProperty;
         public static readonly DependencyProperty ProportionsProperty;
+        public static readonly DependencyProperty AxisVisibleProperty;
+
+        public static readonly DependencyProperty DataColorProperty;
+        public static readonly DependencyProperty AxisColorProperty;
 
         public Point[] Data {
             get { return (Point[])GetValue(DataProperty); }
@@ -28,6 +35,21 @@ namespace SLAM.Views.Controls.Custom {
         public Point Proportions {
             get { return (Point)GetValue(ProportionsProperty); }
             set { SetValue(ProportionsProperty, value); }
+        }
+
+        public bool AxisVisible {
+            get { return (bool)GetValue(AxisVisibleProperty); }
+            set { SetValue(AxisVisibleProperty, value); }
+        }
+
+        public Color DataColor {
+            get { return (Color)GetValue(DataColorProperty); }
+            set { SetValue(DataColorProperty, value); }
+        }
+
+        public Color AxisColor {
+            get { return (Color)GetValue(AxisColorProperty); }
+            set { SetValue(AxisColorProperty, value); }
         }
 
         static MathDataViewport() {
@@ -45,6 +67,21 @@ namespace SLAM.Views.Controls.Custom {
                 FrameworkPropertyMetadataOptions.BindsTwoWayByDefault |
                 FrameworkPropertyMetadataOptions.AffectsRender);
 
+            var axisVisiblePropertyMetadata =
+                new FrameworkPropertyMetadata(true,
+                FrameworkPropertyMetadataOptions.BindsTwoWayByDefault |
+                FrameworkPropertyMetadataOptions.AffectsRender);
+
+            var dataColorPropertyMetadata =
+                new FrameworkPropertyMetadata(Colors.Gray,
+                FrameworkPropertyMetadataOptions.BindsTwoWayByDefault |
+                FrameworkPropertyMetadataOptions.AffectsRender);
+
+            var axisColorPropertyMetadata =
+                new FrameworkPropertyMetadata(Colors.Red,
+                FrameworkPropertyMetadataOptions.BindsTwoWayByDefault |
+                FrameworkPropertyMetadataOptions.AffectsRender);
+
             DataProperty =
                 DependencyProperty.Register(
                     "Data", typeof(Point[]), typeof(MathDataViewport), dataPropertyMetadata);
@@ -52,6 +89,18 @@ namespace SLAM.Views.Controls.Custom {
             ProportionsProperty =
                 DependencyProperty.Register(
                     "Proportions", typeof(Point), typeof(MathDataViewport), proportionsPropertyMetadata);
+
+            AxisVisibleProperty =
+                DependencyProperty.Register(
+                    "AxisVisible", typeof(bool), typeof(MathDataViewport), axisVisiblePropertyMetadata);
+
+            DataColorProperty =
+                DependencyProperty.Register(
+                    "DataColor", typeof(Color), typeof(MathDataViewport), dataColorPropertyMetadata);
+
+            AxisColorProperty =
+                DependencyProperty.Register(
+                    "AxisColor", typeof(Color), typeof(MathDataViewport), axisColorPropertyMetadata);
         }
 
         #endregion        
@@ -65,15 +114,17 @@ namespace SLAM.Views.Controls.Custom {
         public override void OnApplyTemplate() {
             renderer = new PointRenderer();
             viewportSurface = GetTemplateChild(ViewportSurfaceName) as Image;
-            if (viewportSurface != null) {
-                OnRender(null);
-            }
+            axis = GetTemplateChild(XAxisName) as Path;
+            OnRender(null);
         }
 
         protected override void OnRender(DrawingContext drawingContext) {
             viewportSurface.Width = Proportions.X;
             viewportSurface.Height = Proportions.Y;
-            viewportSurface.Source = renderer.TmpRenderMethod(Data);
+            viewportSurface.Source = renderer.Render(Data, DataColor);
+
+            axis.Visibility = AxisVisible ? Visibility.Visible : Visibility.Collapsed;
+            axis.Stroke = new SolidColorBrush(AxisColor);
         }        
     }
 }
--- SLAM/SLAM.Views/Controls/Custom/PointRenderer.cs
+++ SLAM/SLAM.Views/Controls/Custom/PointRenderer.cs
@@ -7,49 +7,67 @@ namespace SLAM.Views.Controls.Custom {
 
     internal sealed class PointRenderer {
 
-        internal ImageSource TmpRenderMethod(Point[] points) {
+        private Point min, max;
 
-            if (points == null || points.Length < 1) { return null; }
+        internal double MinX { get { return min.X; } }
+        internal double MinY { get { return min.Y; } }
 
-            Point min = new Point(points[0].X, points[0].Y);
-            Point max = new Point(points[0].X, points[0].Y);
+        internal double MaxX { get { return max.X; } }
+        internal double MaxY { get { return max.Y; } }
+
+        internal double OffsetX { get; private set; }
+        internal double OffsetY { get; private set; }
+
+        internal ImageSource Render(Point[] newPoints, Color newColor) {
+
+            if (newPoints == null || newPoints.Length < 1) { return null; }
+
+            UpdateLimits(newPoints);
+            return CreateBitmap(newPoints, newColor);
+        }
+
+        private void UpdateLimits(Point[] points) {
+
+            min = new Point(points[0].X, points[0].Y);
+            max = new Point(points[0].X, points[0].Y);
 
             foreach (var point in points) {
-                if (point.X < min.X) { min.X = point.X; }
-                if (point.Y < min.Y) { min.Y = point.Y; }
 
                 if (point.X > max.X) { max.X = point.X; }
+                else
+                if (point.X < min.X) { min.X = point.X; }
+
                 if (point.Y > max.Y) { max.Y = point.Y; }
+                else
+                if (point.Y < min.Y) { min.Y = point.Y; }
             }
+            OffsetX = -min.X;
+            OffsetY = -min.Y;
+        }
 
-            double offsetX = -min.X, offsetY = -min.Y;
+        private ImageSource CreateBitmap(Point[] points, Color color) {
 
-            min.Offset(offsetX, offsetY);
-            max.Offset(offsetX, offsetY);
+            Point localMin = min, localMax = max;
 
-            if (max.X < 1 || max.Y < 1) {
-                return null;
-            }
+            localMin.Offset(OffsetX, OffsetY);
+            localMax.Offset(OffsetX, OffsetY);
 
-            int width = (int)max.X + 1;
-            int height = (int)max.Y + 1;
+            if (localMax.X < 1 || localMax.Y < 1) { return null; }
 
-            WriteableBitmap resultSource
-                = new WriteableBitmap(width, height, 96.0, 96.0, PixelFormats.Bgr32, null);
+            int width = (int)localMax.X + 1;
+            int height = (int)localMax.Y + 1;
 
+            WriteableBitmap result = new WriteableBitmap(width, height, 96.0, 96.0, PixelFormats.Bgr32, null);
             byte[] fullFrameBuffer = new byte[width * height * sizeof(int)];
 
             foreach (var point in points) {
-                point.Offset(offsetX, offsetY);
+                point.Offset(OffsetX, OffsetY);
                 int index = GetLinearIndex((int)point.X, (int)point.Y, width);
-                SetColorToViewportByteArray(fullFrameBuffer, index * sizeof(int), Color.FromArgb(0, 255, 0, 0));
+                SetColorToViewportByteArray(fullFrameBuffer, index * sizeof(int), color);
             }
 
-            resultSource.WritePixels(
-                new Int32Rect(0, 0, width, height),
-                fullFrameBuffer, resultSource.PixelWidth * sizeof(int), 0);
-
-            return resultSource;
+            result.WritePixels(new Int32Rect(0, 0, width, height), fullFrameBuffer, result.PixelWidth * sizeof(int), 0);
+            return result;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
@@ -63,6 +81,6 @@ namespace SLAM.Views.Controls.Custom {
             viewportByteArray[++startIndex] = color.G;
             viewportByteArray[++startIndex] = color.R;
             viewportByteArray[++startIndex] = color.A;
-        }        
+        }
     }
 }
