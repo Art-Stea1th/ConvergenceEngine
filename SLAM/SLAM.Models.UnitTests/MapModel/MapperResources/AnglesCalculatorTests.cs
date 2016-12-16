f6ec8eff4f9d20ac7b0874eb0b3a53f2bd350256
Art.Stea1th (Stanislav Kuzmich) <art.stea1th@gmail.com>
2016-12-16 17:40:21 +0300

    added: first Tests (for AnglesCalculator)

---
+++ SLAM/SLAM.Models.UnitTests/MapModel/MapperResources/AnglesCalculatorTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using SLAM.Models.MapModel.MapperResources;
+using System;
+
+
+namespace SLAM.Models.UnitTests.MapModel.MapperResources {
+
+    [TestFixture]
+    public sealed class AnglesCalculatorTests {
+
+        [Test]
+        [TestCase(3.0, 4.0, 5.0, 36.87)]
+        [TestCase(4.0, 5.0, 3.0, 53.13)]
+        [TestCase(5.0, 3.0, 4.0, 90.00)]
+        public void AlphaAngle_ValidSides_ReturnValidAngle(double sideA, double sideB, double sideC, double expectedAngle) {
+            AnglesCalculator ac = new AnglesCalculator();               // --- Arrange            
+            double resultGamma = ac.AlphaAngle(sideA, sideB, sideC);    // --- Act            
+            Assert.AreEqual(expectedAngle, Math.Round(resultGamma, 2)); // --- Assert
+        }
+
+        [Test]
+        [TestCase(5.0, 4.0, 3.0, 53.13)]
+        [TestCase(4.0, 3.0, 5.0, 36.87)]
+        [TestCase(3.0, 5.0, 4.0, 90.00)]
+        public void BetaAngle_ValidSides_ReturnValidAngle(double sideA, double sideB, double sideC, double expectedAngle) {
+            AnglesCalculator ac = new AnglesCalculator();               // --- Arrange            
+            double resultGamma = ac.BetaAngle(sideA, sideB, sideC);     // --- Act            
+            Assert.AreEqual(expectedAngle, Math.Round(resultGamma, 2)); // --- Assert
+        }
+
+        [Test]
+        [TestCase(3.0, 4.0, 5.0, 90.00)]
+        [TestCase(4.0, 5.0, 3.0, 36.87)]
+        [TestCase(5.0, 3.0, 4.0, 53.13)]
+        public void GammaAngle_ValidSides_ReturnValidAngle(double sideA, double sideB, double sideC, double expectedAngle) {
+            AnglesCalculator ac = new AnglesCalculator();               // --- Arrange            
+            double resultGamma = ac.GammaAngle(sideA, sideB, sideC);    // --- Act            
+            Assert.AreEqual(expectedAngle, Math.Round(resultGamma, 2)); // --- Assert
+        }
+    }
+}
--- SLAM/SLAM.Models.UnitTests/Properties/AssemblyInfo.cs
+++ SLAM/SLAM.Models.UnitTests/Properties/AssemblyInfo.cs
@@ -1,19 +1,18 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("SLAM.Models")]
+[assembly: AssemblyTitle("SLAM.Models.UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SLAM.Models")]
+[assembly: AssemblyProduct("SLAM.Models.UnitTests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
-[assembly: InternalsVisibleTo("SLAM.Models.UnitTests")]
-[assembly: Guid("2e2cbeaf-8d2f-464c-81f3-b6c8411ec8d2")]
+
+[assembly: Guid("c1d7bf97-0aff-4038-8b99-3ef9ee41cc70")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
