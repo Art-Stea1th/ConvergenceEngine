76df3da1cae9dfda50092ace52002d2d48c1a08c
Art.Stea1th (Stanislav Kuzmich) <art.stea1th@gmail.com>
2017-02-24 04:36:57 +0300

    fix: null-reference

--- ConvergenceEngine/ConvergenceEngine.Models.Mapping/Map.cs
+++ ConvergenceEngine/ConvergenceEngine.Models.Mapping/Map.cs
@@ -8,6 +8,7 @@ namespace ConvergenceEngine.Models.Mapping {
 
     using IO.DataExtractors;
     using IO.Readers;
+    using Extensions;
     using Navigation;
 
     public sealed class Map : FrameSequence {
@@ -24,7 +25,12 @@ namespace ConvergenceEngine.Models.Mapping {
         public IEnumerable<Point> FramePoints { get { return ActualFrame?.Points; } }
         public IEnumerable<Tuple<Point, Point>> FrameSegments { get { return ActualFrame?.Select(s => new Tuple<Point, Point>(s.PointA, s.PointB)); } }
         public IEnumerable<Tuple<Point, Point>> PreviousFrameSegments { get { return PreviousFrame?.Select(s => new Tuple<Point, Point>(s.PointA, s.PointB)); } }
-        public IEnumerable<Tuple<Point, Point>> TrackedFrameSegments { get { return ActualFrame?.SelectTrackedTo(PreviousFrame).Select(s => new Tuple<Point, Point>(s.Item2.PointA, s.Item2.PointB)); } }
+        public IEnumerable<Tuple<Point, Point>> TrackedFrameSegments {
+            get {
+                if (ActualFrame.IsNull() || PreviousFrame.IsNull()) { return null; }
+                return ActualFrame.SelectTrackedTo(PreviousFrame).Select(s => new Tuple<Point, Point>(s.Item2.PointA, s.Item2.PointB));
+            }
+        }
         // <-- TMP
 
         internal Map(DataProvider dataProvider) {
